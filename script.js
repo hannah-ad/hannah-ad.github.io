2699f39429d6a9eac1cece657a2c11c3f470484c
hannah-ad <80017806+hannah-ad@users.noreply.github.com>
2021-04-14 10:55:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function math(){
+ var response = prompt("1+1=?");
+ if (response < 2){
+ alert("Try again!");
+}else if (response > 2){
+alert("Try again!");
+}else{
+alert("Bingo!");
+}
+}
+math();
+
+function math2(){
+ var response = prompt("10*10=?");
+ if (response < 100){
+ alert("Try again!");
+}else if (response > 100){
+alert("Try again!");
+}else{
+alert("Bingo!");
+}
+}
+math2();
